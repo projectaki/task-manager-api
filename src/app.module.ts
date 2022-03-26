cd3421f98212969f5517769de157436c2a07c205
Akos <akosegypro@gmail.com>
2022-03-26 13:06:40 +0100

    🪛Scaffold initial REST resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProjectsModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
