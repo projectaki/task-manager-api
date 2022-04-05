16e1728eebb14991779d1a3836705c770a3bd322
Akos <akosegypro@gmail.com>
2022-04-05 19:53:25 +0200

    ➕Create User project schema

---
+++ src/user-projects/schemas/user-project.schema.ts
@@ -0,0 +1,26 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { ProjectRole } from 'src/core/enums/project-role.enum';
+import { Project } from 'src/projects/schemas/project.schema';
+
+export type UserProjectDocument = UserProject & Document;
+
+@Schema({ versionKey: false, timestamps: true, _id: false })
+export class UserProject {
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+
+  @Prop()
+  accepted: boolean;
+
+  @Prop({ enum: ProjectRole })
+  role: ProjectRole;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: Project.name })
+  project: Project | string;
+}
+
+export const UserProjectSchema = SchemaFactory.createForClass(UserProject);
