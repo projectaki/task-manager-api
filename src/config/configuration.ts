0b7106ba5a8c3025fdf8ce71b0cad9c2b74fc00c
Akos <akosegypro@gmail.com>
2022-04-07 22:24:19 +0200

    ➕ add auth to config

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,8 +2,19 @@ export interface DBConfig {
   uri: string;
 }
 
+export interface AuthConfig {
+  authority: string;
+  audience: string;
+  jwksUri: string;
+}
+
 export default () => ({
   database: {
     uri: process.env['MONGODB_URI'],
   } as DBConfig,
+  auth: {
+    authority: process.env.AUTHORITY,
+    audience: process.env.AUDIENCE,
+    jwksUri: process.env.JWKS_URI,
+  } as AuthConfig,
 });
