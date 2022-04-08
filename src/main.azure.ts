c923dbbad86f992a96c1eca016b7af3d33e6ea07
Akos <akosegypro@gmail.com>
2022-04-09 00:47:47 +0200

    🪖Helmet added to middleware

--- src/main.azure.ts
+++ src/main.azure.ts
@@ -1,10 +1,12 @@
 import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 export async function createApp(): Promise<INestApplication> {
   const start = Date.now();
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.setGlobalPrefix('api');
 
   await app.init();
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const start = Date.now();
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.enableCors({
     origin: ['http://localhost:4200'],
   });
